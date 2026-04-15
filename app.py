930e406e1c3b9d330cf0c4d59754530fb0ebf6cb
richardportugais <richardportugais26@gmail.com>
2026-04-15 15:54:05 +0300

    test

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def compress_image(image_bytes: bytes) -> str:
 
 
 # Interface utilisateur Streamlit
-st.title("Formulaire de dépôt Rhino Certification pour Elite Renovation")
+st.title("Formulaire de dépôt Rhino Certification pour Elite Renovationmm")
 
 # Affichage des crédits disponibles
 credits_placeholder = st.empty()
